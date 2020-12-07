b91351e25e4c6f408f7fa45c481b8de1ea2a2a58
Eduard Stankovic <eduard.stankovic.ext@uniqa.at>
2020-12-07 14:22:27 +0100

    Added proper styling

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -19,9 +19,74 @@ export class Tab1Page {
     }
   };
 
+  slides: {name: string; image: string, color: string}[] = [
+    {
+      name: 'Nike',
+      image: 'assets/images/slide-1.png',
+      color: '0, 140, 255'
+    },
+    {
+      name: 'Nike',
+      image: 'assets/images/slide-2.png',
+      color: '236, 218, 196'
+    }
+  ]
+
+  products: {name: string, image: string, liked?: boolean}[] = [
+    {
+      name: 'Ceramic Cup',
+      image: 'assets/images/product-2.png'
+    },
+    {
+      name: 'Nike Shoes',
+      image: 'assets/images/product-1.png',
+      liked: true
+    },
+    {
+      name: 'iPhone 11',
+      image: 'assets/images/product-3.png'
+    },
+    {
+      name: 'Towel',
+      image: 'assets/images/product-4.png'
+    },
+    {
+      name: 'iPhone 11',
+      image: 'assets/images/product-3.png'
+    },
+    {
+      name: 'Ceramic Cup',
+      image: 'assets/images/product-2.png'
+    },
+    {
+      name: 'Nike Shoes',
+      image: 'assets/images/product-1.png'
+    },
+    {
+      name: 'iPhone 11',
+      image: 'assets/images/product-3.png'
+    },
+    {
+      name: 'Towel',
+      image: 'assets/images/product-4.png'
+    },
+  ];
+
   constructor(private readonly router: Router) {}
 
   openDetail() {
     this.router.navigate(['/product-detail']);
+  }
+
+  getBgImageString(img: string) {
+    return `url('${img}')`
+  }
+
+  getBgColorString(rgb: string) {
+    return `rgb(${rgb})`
+  }
+
+  getBgShadowString(rgb: string) {
+    return `0px 6px 17px 0px rgba(${rgb},0.66)`
   }
 }
